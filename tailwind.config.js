7cf97d66a68301e81cabb5e22d74b93b9679e8cc
Benjamin Gonzalez <boomdigital@MacBook-Air-Benja.local>
2025-10-22 03:54:57 -0300

    Update Tailwind config format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+tailwind.config = {
   theme: {
     extend: {
       colors: {
@@ -36,7 +31,6 @@ export default {
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       },
-    },
-  },
-  plugins: [],
+    }
+  }
 }
